734eb700245fc05d4806644d39b3d6c34eef5c49
ACER <assonshrestha99@gmail.com>
2021-06-10 14:25:16 +0545

    printing commit

---
+++ Lab1/Ono5.py
@@ -0,0 +1,6 @@
+name=input('Enter your name:')
+age=int(input('Enter your age:'))
+
+
+print('My name is ' + name + ' and I am ' + str(age) + 'years old')
+print('My name is',name,' and I am ',age, 'years old ')
